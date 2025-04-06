a1df4cc428237bddafc12279720568f4d6969cf0
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-06 23:21:19 +0545

    get categories in dao

--- src/main/java/com/collegetutorial/controller/dao/ProductDAO.java
+++ src/main/java/com/collegetutorial/controller/dao/ProductDAO.java
@@ -2,9 +2,12 @@ package com.collegetutorial.controller.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import com.collegetutorial.controller.database.DatabaseConnection;
+import com.collegetutorial.model.Category;
 import com.collegetutorial.model.Products;
 
 public class ProductDAO {
@@ -40,6 +43,27 @@ public class ProductDAO {
 	        }
 	        return isRowInserted;
 	    }
-	    
+	    // Get All Categories
+	    public ArrayList<Category> getAllCategories() throws SQLException {
+	        ArrayList<Category> categoryList = new ArrayList<>();
+	        String sql = "SELECT category_id, name, description, created_at FROM categories ORDER BY name";
+	        
+	        if(conn != null) {
+	            ps = conn.prepareStatement(sql);
+	            ResultSet categorySet = ps.executeQuery();
+	            
+	            while (categorySet.next()) {
+	                Category category = new Category(
+	                    categorySet.getInt("category_id"),
+	                    categorySet.getString("name"),
+	                    categorySet.getString("description"),
+	                    categorySet.getTimestamp("created_at")
+	                );
+	                categoryList.add(category);
+	            }
+	        }
+	        
+	        return categoryList;
+	    }    
 	    
 }
