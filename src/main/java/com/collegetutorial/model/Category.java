01228f9d1cebd17ef29b006892115239f5c36101
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-06 21:11:41 +0545

    added category model

---
+++ src/main/java/com/collegetutorial/model/Category.java
@@ -0,0 +1,41 @@
+package com.collegetutorial.model;
+
+public class Category {
+
+	    private int categoryId;
+	    private String name;
+	    private String description;
+
+	    public Category(int categoryId, String name, String description) {
+	        this.categoryId = categoryId;
+	        this.name = name;
+	        this.description = description;
+	    }
+
+	    // Getters & Setters
+	    public int getCategoryId() {
+	        return categoryId;
+	    }
+
+	    public void setCategoryId(int categoryId) {
+	        this.categoryId = categoryId;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+
+	    public void setName(String name) {
+	        this.name = name;
+	    }
+
+	    public String getDescription() {
+	        return description;
+	    }
+
+	    public void setDescription(String description) {
+	        this.description = description;
+	    }
+	
+
+}
