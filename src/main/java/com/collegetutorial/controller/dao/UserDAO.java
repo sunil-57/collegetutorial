5bcef86eb77bde8239794fbd52c145ac26150513
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-06 21:10:30 +0545

    added log in method

--- src/main/java/com/collegetutorial/controller/dao/UserDAO.java
+++ src/main/java/com/collegetutorial/controller/dao/UserDAO.java
@@ -7,54 +7,100 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 import com.collegetutorial.controller.database.DatabaseConnection;
-import com.collegetutorial.model.Student;
+import com.collegetutorial.model.User;
 
-public class StudentDAO {
+public class UserDAO {
 	private Connection conn;
 	private PreparedStatement ps;
 	
-	public StudentDAO() throws ClassNotFoundException, SQLException {
+	public UserDAO() throws ClassNotFoundException, SQLException {
 		this.conn = DatabaseConnection.getConnection();
 	}
+	//User registration
+	 public boolean register(User user){
+		 	boolean isUserRegistered = false;
+	        String query = "INSERT INTO users (name, email, password, phone, address, role) VALUES (?, ?, ?, ?, ?, ?)";
+	        if(conn != null) {
+	        	
+					try {
+						ps = conn.prepareStatement(query);
+						 ps.setString(1, user.getName());
+				            ps.setString(2, user.getEmail());
+				            ps.setString(3, user.getPassword()); // TODO Should be hashed
+				            ps.setString(4, user.getPhone());
+				            ps.setString(5, user.getAddress());
+				            ps.setString(6, user.getRole());
+				            if(ps.executeUpdate() > 0) {
+				            	isUserRegistered = true;
+				            }
+					} catch (SQLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+	        }
+	        return isUserRegistered;
+	        
+	    }
 	
-	public boolean addStudent(Student student) {
-		boolean isRowInserted = false;
-		String query = "INSERT INTO student(username, password)"
-				+"VALUES (?,?)";
+	 // get all the users from database
+	public ArrayList<User> getAllUsers() {
+		ArrayList<User> users = new ArrayList<>();
+		String query = "SELECT * FROM users";
 		if(conn != null) {
 			try {
 				ps = conn.prepareStatement(query);
-				ps.setString(1, student.getUsername());
-				ps.setString(2, student.getPassword());
-				int rows = ps.executeUpdate();
-				if(rows > 0) {
-					isRowInserted = true;
-				}
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}
-		return isRowInserted;
-	}
-	public ArrayList<Student> getStudents() {
-		ArrayList<Student> students = new ArrayList<>();
-		String query = "SELECT * FROM student";
-		if(conn != null) {
-			try {
-				ps = conn.prepareStatement(query);
-				ResultSet studentSet = ps.executeQuery();
-				while(studentSet.next()) {
-					String username = studentSet.getString("username");
-					String password = studentSet.getString("password");
-					Student student = new Student(username,password);
-					students.add(student);
+				ResultSet userSet = ps.executeQuery();
+				while(userSet.next()) {
+					 User user = new User();
+			            user.setUserId(userSet.getInt("user_id"));
+			            user.setName(userSet.getString("name"));
+			            user.setEmail(userSet.getString("email"));
+			            user.setPassword(userSet.getString("password"));
+			            user.setPhone(userSet.getString("phone"));
+			            user.setAddress(userSet.getString("address"));
+			            user.setRole(userSet.getString("role"));
+			            user.setCreatedAt(userSet.getTimestamp("created_at"));
+			            users.add(user);
+					users.add(user);
 				}
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
-		return students;
+		return users;
 	}
+	
+	//get user log info
+	public User login(String email, String password) {
+		User user = null;
+        String query = "SELECT * FROM users WHERE email = ? AND password = ?";
+        if(conn != null) {
+        	try {
+            	ps = conn.prepareStatement(query);
+                ps.setString(1, email);
+                ps.setString(2, password); // NOTE: Password should be hashed in production
+                ResultSet userSet = ps.executeQuery();
+
+                if (userSet.next()) {
+                	new User(
+                			userSet.getInt("user_id"),
+                			userSet.getString("name"),
+                			userSet.getString("email"),
+                			userSet.getString("password"),
+                			userSet.getString("phone"),
+                			userSet.getString("address"),
+                			userSet.getString("role"),
+                			userSet.getTimestamp("created_at")
+                        );
+                }
+            }catch(SQLException e) {
+            	// TODO Auto-generated catch block
+    			e.printStackTrace();
+            }
+        }
+        return user;
+    }
+	
 	
 }
