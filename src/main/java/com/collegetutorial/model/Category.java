1b0c6e5deff56291cfb4a1706bcc380d61b31e5e
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-06 22:44:37 +0545

    added created at in category

--- src/main/java/com/collegetutorial/model/Category.java
+++ src/main/java/com/collegetutorial/model/Category.java
@@ -1,41 +1,44 @@
 package com.collegetutorial.model;
 
+import java.sql.Timestamp;
+
 public class Category {
 
 	    private int categoryId;
 	    private String name;
 	    private String description;
+	    private Timestamp createdAt;
+		public Category(int categoryId, String name, String description, Timestamp createdAt) {
+			super();
+			this.categoryId = categoryId;
+			this.name = name;
+			this.description = description;
+			this.createdAt = createdAt;
+		}
+		public int getCategoryId() {
+			return categoryId;
+		}
+		public void setCategoryId(int categoryId) {
+			this.categoryId = categoryId;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getDescription() {
+			return description;
+		}
+		public void setDescription(String description) {
+			this.description = description;
+		}
+		public Timestamp getCreatedAt() {
+			return createdAt;
+		}
+		public void setCreatedAt(Timestamp createdAt) {
+			this.createdAt = createdAt;
+		}
 
-	    public Category(int categoryId, String name, String description) {
-	        this.categoryId = categoryId;
-	        this.name = name;
-	        this.description = description;
-	    }
-
-	    // Getters & Setters
-	    public int getCategoryId() {
-	        return categoryId;
-	    }
-
-	    public void setCategoryId(int categoryId) {
-	        this.categoryId = categoryId;
-	    }
-
-	    public String getName() {
-	        return name;
-	    }
-
-	    public void setName(String name) {
-	        this.name = name;
-	    }
-
-	    public String getDescription() {
-	        return description;
-	    }
-
-	    public void setDescription(String description) {
-	        this.description = description;
-	    }
-	
-
+	    
 }
