bbde11199d58171778b14ddb0240e383e276a9ec
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-12 20:00:26 +0545

    comments in categoryDAO

--- src/main/java/com/collegetutorial/controller/dao/CategoryDAO.java
+++ src/main/java/com/collegetutorial/controller/dao/CategoryDAO.java
@@ -12,50 +12,51 @@ import com.collegetutorial.model.Category;
 public class CategoryDAO {
 	private Connection conn;
 	private PreparedStatement ps;
+
 	public CategoryDAO() throws ClassNotFoundException, SQLException {
-		conn = DatabaseConnection.getConnection();
+		conn = DatabaseConnection.getConnection();// Establish a database connection when CategoryDAO is created
 	}
-    // creating a new category
-    public boolean addCategory(Category category) {
-    	boolean isRowInserted = false;
-        String sql = "INSERT INTO categories (name, description) VALUES (?, ?)";
-        
-        if(conn != null) {
-        	try {
-                ps = conn.prepareStatement(sql); 
-                ps.setString(1, category.getName());
-                ps.setString(2, category.getDescription());
-                int rows = ps.executeUpdate();
-                if(rows > 0) {
-                	isRowInserted = true;
-                }
-                return isRowInserted;
-            } catch  (SQLException e) {
-    			// TODO Auto-generated catch block
-    			e.printStackTrace();
-            }
-        }
+
+	// Add a new category to the database
+	public boolean addCategory(Category category) {
+		boolean isRowInserted = false;
+		String sql = "INSERT INTO categories (name, description) VALUES (?, ?)";
+
+		if (conn != null) {
+			try {
+				ps = conn.prepareStatement(sql);
+				ps.setString(1, category.getName());
+				ps.setString(2, category.getDescription());
+				int rows = ps.executeUpdate();
+				if (rows > 0) {
+					isRowInserted = true;
+				}
+				return isRowInserted;
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 		return isRowInserted;
-      }
-    
-    // get All Categories
-    public ArrayList<Category> getAllCategories() throws SQLException {
-    	ArrayList<Category> categoryList = new ArrayList<>();
-        String sql = "SELECT category_id, name, description, created_at FROM categories";
-        if(conn != null) {
-        	ps = conn.prepareStatement(sql);
-    		ResultSet categorySet = ps.executeQuery(sql);
-    		while (categorySet.next())  {
-    		        Category category = new Category(
-    		        		categorySet.getInt("category_id"),
-    		        		categorySet.getString("name"),
-    		        		categorySet.getString("description"),
-    		        		categorySet.getTimestamp("created_at")
-    		        );
-    		    categoryList.add(category);
-    		}
-        }
-        
-        return categoryList;
-    }
+	}
+
+	// get All Categories
+	public ArrayList<Category> getAllCategories() throws SQLException {
+		ArrayList<Category> categories = new ArrayList<>();
+		String sql = "SELECT category_id, name, description, created_at FROM categories";
+		if (conn != null) {
+			ps = conn.prepareStatement(sql);
+			ResultSet categorySet = ps.executeQuery(sql);
+			while (categorySet.next()) {
+				Category category = new Category(
+						categorySet.getInt("category_id"), 
+						categorySet.getString("name"),
+						categorySet.getString("description"), 
+						categorySet.getTimestamp("created_at"));
+				categories.add(category);
+			}
+		}
+
+		return categories;
+	}
 }
