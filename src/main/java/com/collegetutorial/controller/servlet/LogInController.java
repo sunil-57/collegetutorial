1bfda7f5b98be60817173e81cd1b8a4310e727d4
Sunil Thapa <sunilthapa056@outlook.com>
2025-04-13 23:21:36 +0545

    solved incorrect password

--- src/main/java/com/collegetutorial/controller/servlet/LogInController.java
+++ src/main/java/com/collegetutorial/controller/servlet/LogInController.java
@@ -59,22 +59,22 @@ public class LogInController extends HttpServlet {
 				// Create a session for the logged-in user
 				HttpSession session = request.getSession();
 				session.setAttribute("userWithSession", user);
-				session.setMaxInactiveInterval(10);
+				session.setMaxInactiveInterval(60*60);
 				// Redirect to the Dashboard or home page (could be a different page based on
 				// your setup)
 				response.sendRedirect(request.getContextPath() + "/pages/Dashboard.jsp");
 
 			} else {
 				// If login fails, send an error message to the log in page
-				request.setAttribute("login_garda_aako_errorMessage", "Invalid email or password. Please try again.");
-				request.getRequestDispatcher(request.getContextPath() + "/pages/login.jsp").forward(request, response);
+				request.setAttribute("errorMessage", "Invalid email or password. Please try again.");
+				request.getRequestDispatcher("/pages/Login.jsp").forward(request, response);
 			}
 			// donot use the parameter and attribute name in the above manner.
 			// In this example, it is done just for students such that it becomes easier for understanding	
 		} catch (ClassNotFoundException | SQLException e) {
 			// Log the error properly in production (use a logger)
 			request.setAttribute("errorMessage", "A system error occurred. Please try again later.");
-			request.getRequestDispatcher(request.getContextPath() +"/pages/login.jsp").forward(request, response);
+			request.getRequestDispatcher("/pages/Login.jsp").forward(request, response);
 		} finally {
 			if (out != null) {
 				out.close();
